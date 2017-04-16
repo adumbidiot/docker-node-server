09aad52c5f5bc9aa6ecd796d0433a0dbdef45dba
adumbidiot <nathaniel.daniel23@outlook.com>
2017-04-15 18:00:24 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const PORT = '9002';
+
+app.use(function(req, res, next){
+	console.log(req.path);
+	next();
+});
+
+app.use(function(req, res){
+	res.send('<html><body><p>UNDER DEVELOPMENT!</p><a href = "www.nanopi.ml">Try Here in the meantime!</a></body></html>');
+});
+
+app.listen(PORT);
