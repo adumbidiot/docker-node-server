67ddfd7305ee37df0204ce08db69fbefedb4a8d9
adumbidiot <nathaniel.daniel23@outlook.com>
2017-04-15 18:49:02 -0700

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(function(req, res, next){
 });
 
 app.use(function(req, res){
-	res.send('<html><body><p>UNDER DEVELOPMENT!</p><a href = "www.nanopi.ml">Try Here in the meantime!</a></body></html>');
+	res.sendFile(__dirname + '/index.html');
 });
 
 app.listen(PORT);
