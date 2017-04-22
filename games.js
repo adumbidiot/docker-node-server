bb641e8871a815c504d7323a21af13736e45e0a8
adumbidiot <nathaniel.daniel23@outlook.com>
2017-04-21 21:52:49 -0700

    Update games.js

--- games.js
+++ games.js
@@ -1,5 +1,18 @@
 const express = require('express');
 const app = express();
+const handlebars = require('./handlebars');
+
+app.engine('.hbs', handlebars.engine);
+app.set('view engine', '.hbs');
+app.enable('view cache');
+
+app.get('/', function(req, res){
+	res.render('games');
+});
+
+app.get('/platformer', function(req, res){
+	res.render('platformer');
+});
 
 app.get('/platformer/logic.js', function(req, res){
 	res.sendFile(__dirname + '/public/logic.js');
