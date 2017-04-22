c1548c4cee41243228e8049e61f6220e331daf0b
adumbidiot <nathaniel.daniel23@outlook.com>
2017-04-22 14:56:50 -0700

    Update logic.js

--- public/games/logic.js
+++ public/games/logic.js
@@ -3,7 +3,7 @@ var past = 0;
 function time(data){
 	console.log(data);
 	if(past === data && past !== 0){
-		$.post("score", {
+		$.post("platformer/score", {
 			time: atob(data)
     		},
     		function(data, status){
