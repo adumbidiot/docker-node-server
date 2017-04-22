e2d3816a64db5cf3a1de5dd73aee9c40daf40487
adumbidiot <nathaniel.daniel23@outlook.com>
2017-04-21 21:44:46 -0700

    Update games.js

--- games.js
+++ games.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const app = express();
 
-app.get('/', function(req, res){
- 
+app.get('/platformer/logic.js', function(req, res){
+	res.sendFile(__dirname + '/public/logic.js');
+});
+
+app.get('/platformer/test.swf', function(req, res){
+	res.sendFile(__dirname + '/public/test.swf');
 });
 
 module.exports = app;
