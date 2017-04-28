06ce54a1a2f37dbdc29e5d17e5cac47351801a81
adumbidiot <nathaniel.daniel23@outlook.com>
2017-04-27 21:56:22 -0700

    Update login.js

--- login.js
+++ login.js
@@ -3,4 +3,21 @@ const app = express();
 const jwt = require('jsonwebtoken');
 const bodyparser = require('body-parser');
 const cookieparser = require('cookie-parser');
+const handlebars = require('./handlebars');
 const secret = 'thecakewasalie';
+
+app.engine('.hbs', handlebars.engine);
+app.set('view engine', '.hbs');
+app.enable('view cache');
+
+app.use(bodyparser.urlencoded({
+  extended: true
+}));
+
+app.use(cookieparser());
+
+app.get('/', function(req, res, next){
+  res.render(login, {login: true});
+});
+
+module.exports = app;
