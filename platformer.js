af1c47581b0c084cf5beb716e7ba88b01d3f9185
adumbidiot <nathaniel.daniel23@outlook.com>
2017-04-27 20:07:36 -0700

    Update platformer.js

--- platformer.js
+++ platformer.js
@@ -5,7 +5,7 @@ app.get('/logic.js', function(req, res){
 	res.sendFile(__dirname + '/public/games/logic.js');//Fix file name plz 
 });
 
-app.get('r/platformer.jpg', function(req, res){
+app.get('/platformer.jpg', function(req, res){
 	res.sendFile(__dirname + '/public/games/platformer.jpg');
 });
 
