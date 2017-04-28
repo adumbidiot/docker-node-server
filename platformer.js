4d2628b6b1dd71b1336d71380d9803b8d48980ff
adumbidiot <nathaniel.daniel23@outlook.com>
2017-04-27 20:06:57 -0700

    Update platformer.js

--- platformer.js
+++ platformer.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const app = express();
 
-app.get('/platformer/logic.js', function(req, res){
+app.get('/logic.js', function(req, res){
 	res.sendFile(__dirname + '/public/games/logic.js');//Fix file name plz 
 });
 
-app.get('/platformer/platformer.jpg', function(req, res){
+app.get('r/platformer.jpg', function(req, res){
 	res.sendFile(__dirname + '/public/games/platformer.jpg');
 });
 
-app.get('/platformer/test.swf', function(req, res){
+app.get('/test.swf', function(req, res){
 	res.sendFile(__dirname + '/public/loader.swf'); //TODO: Clean code and rename SWF
 });
 
