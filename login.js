bdf6b8fb576dc43c48f4829ee2b8e4ad1dd1d98c
adumbidiot <nathaniel.daniel23@outlook.com>
2017-04-27 22:14:36 -0700

    Update login.js

--- login.js
+++ login.js
@@ -16,8 +16,30 @@ app.use(bodyparser.urlencoded({
 
 app.use(cookieparser());
 
-app.get('/', function(req, res, next){
+app.use(function(req, res, next){
+  jwt.verify(req.cookies.auth, secret, function(err, decoded){
+    if(err) return next();
+    return res.redirect('/');
+  });
+});
+
+app.get('/', function(req, res){
   res.render('login', {login: true});
+});
+
+app.post('/', function(req, res){
+  var profile = {
+    username: 'admin',
+    password: 'password'
+  }
+  
+  if(req.body.username === profile.username && req.body.password === profile.password){
+    var token = jwt.sign(profile, secret);
+    res.cookie('auth', token, {expires: new Date(Date.now() + 900000), httpOnly: true, secure: true});
+    res.redirect('/');
+  }else{
+    res.redirect('login');
+  }
 });
 
 module.exports = app;
