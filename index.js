3b8710c573bb7b7add4892a437c1f1b92d9f7441
adumbidiot <nathaniel.daniel23@outlook.com>
2017-04-27 21:57:07 -0700

    Update index.js

--- index.js
+++ index.js
@@ -34,6 +34,7 @@ app.use(function(req, res, next){
 
 app.use('/games', games);
 app.use('/api', api);
+app.use('/login', login);
 
 app.get('/', function(req, res){
 	res.render('index', {home: true});
