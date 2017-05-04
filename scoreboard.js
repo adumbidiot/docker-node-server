f05b4fee24cf594aefd2591c0548499535711085
adumbidiot <nathaniel.daniel23@outlook.com>
2017-05-04 13:08:37 -0700

    Update scoreboard.js

--- scoreboard.js
+++ scoreboard.js
@@ -1,5 +1,10 @@
 const pg = require('pg');
 
 const client = new pg.Client('postgres://192.168.1.13');
+client.connect(function(err){
+ if(err){
+   console.log(err);
+ }
+});
 
 module.exports = client;
