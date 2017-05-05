d3efd421bbc5842dda7930003c24b2c510a33033
adumbidiot <nathaniel.daniel23@outlook.com>
2017-05-05 08:18:17 -0700

    Update handlebars.js

--- handlebars.js
+++ handlebars.js
@@ -2,6 +2,8 @@ const exhbs = require('express-handlebars');
 const config = require('./config');
 
 module.exports = exhbs.create(config.exhbs);
-module.exports.attach = function(){
-  
+module.exports.attach = function(array){
+  for(var i in array){
+    console.log(array[i]);
+  }
 }
