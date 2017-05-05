c3e808772825b4b7fe059b696637d7155b842300
adumbidiot <nathaniel.daniel23@outlook.com>
2017-05-05 13:36:32 -0700

    Update api.js

--- api.js
+++ api.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const app = express();
+const request = require('request');
+
+app.get('/Platformer_60fps.swf', function(req, res){
+	request.get('https://sctiger1311.github.io/Skeleton-Sprint/Platformer_60fps.swf').pipe(res);	
+});
 
 app.use(function(req, res){
 	res.redirect('https://www.nanopi.ml' + req.path);
