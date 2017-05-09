e579f8dc1d02808c7bdcabc1d5675175921749f3
adumbidiot <nathaniel.daniel23@outlook.com>
2017-05-09 15:57:17 -0700

    Update games.js

--- games.js
+++ games.js
@@ -107,7 +107,7 @@ app.get('/moomoo.io/bundle.js', function(req, res){
 		headers['transfer-encoding'] = 'chunked';
 		delete headers['content-length'];
 		res.writeHead(200, headers);
-		//response/*.pipe(through2(function(chunk, enc, cb){
+		response.pipe(through2(function(chunk, enc, cb){
 			var data = chunk;
 			if(data.indexOf('\x68\x74\x74\x70\x3A') != -1){
 				var i = data.indexOf('\x68\x74\x74\x70\x3A');
@@ -115,12 +115,12 @@ app.get('/moomoo.io/bundle.js', function(req, res){
 				var buf1 = data.slice(0, i + 4);
 				var buf2 = Buffer.from('\x73', 'utf8');
 				var buf3 = data.slice(i + 4, data.length); 
-				//data = Buffer.concat([buf1, buf2, buf3]);
+				data = Buffer.concat([buf1, buf2, buf3]);
 			}
 			this.push(chunk);
 			cb();
-		//}))*/.pipe(res);
-		response.pipe(res);
+		})).pipe(res);
+		//response.pipe(res);
 	}).on('error', console.error);	
 });
 
