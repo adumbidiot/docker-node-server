29e0770b20d289880cec6ef80b90f22ff1051fca
adumbidiot <nathaniel.daniel23@outlook.com>
2017-05-09 14:10:06 -0700

    Update games.js

--- games.js
+++ games.js
@@ -115,7 +115,7 @@ app.get('/moomoo.io/bundle.js', function(req, res){
 				console.log(chunk.toString('utf8'));
 				var buf1 = data.slice(0, i);
 				var buf2 = Buffer.from('\x73');
-				var buf3 = data.slice(i, data.length); 
+				var buf3 = data.slice(i + 4, data.length); 
 				data = Buffer.concat([buf1, buf2, buf3]);
 				c++;
 			}
