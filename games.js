5f02006d8a461b92dc01a8aa9ec12486420e342f
adumbidiot <nathaniel.daniel23@outlook.com>
2017-05-08 20:32:55 -0700

    Update games.js

--- games.js
+++ games.js
@@ -69,17 +69,17 @@ app.get('/moomoo.io', function(req, res){
 				var j = chunk.indexOf('"', i + 21);
 				ip = chunk.slice(i + 21, j).toString('utf8');
 				console.log(ip);
-				var buf1 = chunk.slice(0, i + 21);
-				var buf2 = Buffer.from('nanopi.ml/games/moomoo.io/bundle.js');
-				var buf3 = chunk.slice(j, chunk.length);
-				data = Buffer.concat([buf1, buf2, buf3], buf1.length + buf2.length + buf3.length); 
+				//var buf1 = chunk.slice(0, i + 21);
+				//var buf2 = Buffer.from('nanopi.ml/games/moomoo.io/bundle.js');
+				//var buf3 = chunk.slice(j, chunk.length);
+				//data = Buffer.concat([buf1, buf2, buf3], buf1.length + buf2.length + buf3.length); 
 			}
 			
-			if(data.indexOf('script.src = "http') != -1){
-				var i = data.indexOf('script.src = "http');
-				var buf1 = data.slice(0, i + 18);
-				var buf2 = Buffer.from('s');
-				var buf3 = data.slice(i + 18, chunk.length);
+			if(data.indexOf('"http://" + serverAddress + ":3000/bundle.js"') != -1){
+				var i = data.indexOf('"http://" + serverAddress + ":3000/bundle.js"');
+				var buf1 = data.slice(0, i + 45);
+				var buf2 = Buffer.from('https://nanopi.ml/games/moomoo.io/bundle.js');
+				var buf3 = data.slice(i + 45, chunk.length);
 				data =  Buffer.concat([buf1, buf2, buf3], buf1.length + buf2.length + buf3.length); 
 			}
 			
