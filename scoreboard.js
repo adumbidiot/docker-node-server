4f1aac76cc25654fc7c155ca55a7059fc649e6a4
adumbidiot <nathaniel.daniel23@outlook.com>
2017-05-10 15:16:03 -0700

    Update scoreboard.js

--- scoreboard.js
+++ scoreboard.js
@@ -1,10 +1,16 @@
-//const pg = require('pg');
+const client = require('mongodb').MongoClient;
+const url = 'mongodb://192.168.1.11:27017/scoreboard';
 
-//const client = new pg.Client('postgres://192.168.1.16');
-//client.connect(function(err){
- //if(err){
-  // console.log(err);
-// }
-//});
+client.connect(url, function(err, db) {
+  var collection = db.collection('skeleton-sprint');
+  collection.insertOne({scores: [1, 2, 3, 4, 5]});
+  db.close();
+});
 
-//module.exports = client;
+client.connect(url, function(err, db) {
+  var collection = db.collection('skeleton-sprint');
+  collection.find().next(function(err, doc){
+   console.log(doc);
+   db.close();
+  });
+});
