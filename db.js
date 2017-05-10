746873b7673340129f21d160083d7ef7e60936f2
adumbidiot <nathaniel.daniel23@outlook.com>
2017-05-10 16:09:56 -0700

    Update and rename scoreboard.js to db.js

--- db.js
+++ db.js
@@ -1,17 +1,17 @@
 const client = require('mongodb').MongoClient;
-const url = 'mongodb://192.168.1.11:27017/scoreboard';
+const url = 'mongodb://192.168.1.11:27017/games';
 
 client.connect(url, function(err, db) {
   if(err) return console.error(err);
   var collection = db.collection('skeleton-sprint');
-  collection.insertOne({scores: [1, 2, 3, 4, 5]});
+  collection.insertOne({scoreboard: [1, 2, 3, 4, 5]});
   db.close();
 });
 
 client.connect(url, function(err, db) {
   var collection = db.collection('skeleton-sprint');
   collection.find().next(function(err, doc){
-   console.log(doc);
+   console.log(doc.scoreboard);
    db.close();
   });
 });
