9a356c500d6a2da5cf45e7d4daa432c100f816a7
adumbidiot <nathaniel.daniel23@outlook.com>
2017-05-10 16:14:55 -0700

    Update platformer.js

--- platformer.js
+++ platformer.js
@@ -1,5 +1,45 @@
 const express = require('express');
 const app = express();
+const bodyparser = require('body-parser');
+var scores = [
+	{name: 'NONE', score: '999.999'},
+	{name: 'NONE ', score: '999.999'},
+	{name: 'NONE', score: '999.999'},
+	{name: 'NONE', score: '999.999'},
+	{name: 'NONE', score: '999.999'},
+	{name: 'NONE', score: '999.999'},
+	{name: 'NONE', score: '999.999'},
+	{name: 'NONE', score: '999.999'},
+	{name: 'NONE', score: '999.999'},
+	{name: 'NONE', score: '999.999'}
+];
+
+app.use(bodyparser.urlencoded({
+  extended: true
+}));
+
+app.post('/platformer/score', function(req, res){
+	//Recieve Response
+	//TODO: Ignore values after 2nd decimal
+	var score = req.body.time;
+	var name = req.body.name;//Convieniece
+	console.log(score + ' | ' + name); //WOW!!! Basic logging!
+	res.send('ok'); //TODO: ADD errors and states
+	
+	//Order score list
+	for(var i = 0; i != scores.length; i++){
+		if(Number(score) < Number(scores[i].score)){
+			scores.splice(i, 0, {name: name, score: score}); //insert score
+			scores.length = 10; //Prune to 10
+			return; //end function
+		}
+	}
+	scores.length = 10; //Prune to 10 (Do I really need to prune if no data is added?)
+});
+
+app.get('/platformer/score', function(req, res){
+	res.json(scores);
+});
 
 app.get('/', function(req, res){
 	res.render('platformer', {games: true, auth: req.auth});
