f0476cfe0e77c552053024ea3b27f37e2680c2bc
adumbidiot <nathaniel.daniel23@outlook.com>
2017-05-10 15:24:57 -0700

    Update scoreboard.js

--- scoreboard.js
+++ scoreboard.js
@@ -2,6 +2,7 @@ const client = require('mongodb').MongoClient;
 const url = 'mongodb://192.168.1.11:27017/scoreboard';
 
 client.connect(url, function(err, db) {
+  if(err) return console.error(err);
   var collection = db.collection('skeleton-sprint');
   collection.insertOne({scores: [1, 2, 3, 4, 5]});
   db.close();
