e98768f00217bddc46f91b604fc89748a3c7faa7
adumbidiot <nathaniel.daniel23@outlook.com>
2017-05-10 17:27:10 -0700

    Update db.js

--- db.js
+++ db.js
@@ -1,34 +1,6 @@
 const client = require('mongodb').MongoClient;
 const url = 'mongodb://192.168.1.11:27017/games';
-/*
-client.connect(url, function(err, db) {
-  if(err) return console.error(err);
-  var collection = db.collection('skeleton-sprint');
-  collection.updateOne({}, {$set: {scoreboard: 
-			[
-				{name: 'NONE', score: '9999.9999'},
-				{name: 'NONE', score: '9999.9999'},
-				{name: 'NONE', score: '9999.9999'},
-				{name: 'NONE', score: '9999.9999'},
-				{name: 'NONE', score: '9999.9999'},
-				{name: 'NONE', score: '9999.9999'},
-				{name: 'NONE', score: '9999.9999'},
-				{name: 'NONE', score: '9999.9999'},
-				{name: 'NONE', score: '9999.9999'},
-				{name: 'NONE', score: '9999.9999'}
-			]}});
-  			db.close();
-});
-*/
-/*
-client.connect(url, function(err, db) {
-  var collection = db.collection('skeleton-sprint');
-  collection.find().next(function(err, doc){
-   console.log(doc.scoreboard);
-   db.close();
-  });
-});
-*/
+
 module.exports = client;
 module.exports.scoreboard = {};
 module.exports.scoreboard.skeletonsprint = {};
