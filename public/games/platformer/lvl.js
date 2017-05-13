7c481d5e1747b736af5f9c0073867bc3e9f37d17
adumbidiot <nathaniel.daniel23@outlook.com>
2017-05-13 14:57:41 -0700

    Create lvl.js

---
+++ public/games/platformer/lvl.js
@@ -0,0 +1,8 @@
+window.lvl = function(){
+      var d = document.createElement("div");
+			d.style.width = "23px";
+			d.style.height = "23px";
+			d.style.border = "1px solid black";
+			d.style.float = "left";
+      this.gridTemplate = d;
+}
