64541700b3859e57ba21ed6f0cc38c21065c3969
adumbidiot <nathaniel.daniel23@outlook.com>
2017-05-13 15:27:40 -0700

    Update lvl.js

--- public/games/platformer/lvl.js
+++ public/games/platformer/lvl.js
@@ -1,9 +1,14 @@
 window.lvl = function(){
-	this.gridTemplate = document.createElement("div");
-	this.gridTemplate.style.width = "23px";
-	this.gridTemplate.style.height = "23px";
-	this.gridTemplate.style.border = "1px solid black";
-	this.gridTemplate.style.float = "left";
+	this.gridTemplate = document.createElement('div');
+	this.gridTemplate.style.width = '23px';
+	this.gridTemplate.style.height = '23px';
+	this.gridTemplate.style.border = '1px solid black';
+	this.gridTemplate.style.float = 'left';
+	
+	this.board = document.createElement('div');
+	this.board.id = 'main';
+	this.board.style.width = '800px';
+	this.board.height = '450px';
 }
 
 window.lvl.mouseDown = false;
