baa0ce51d0b5c2d6feae3660cfdbe16ce3b16968
adumbidiot <nathaniel.daniel23@outlook.com>
2017-05-18 21:51:02 -0700

    Update lvl.js

--- public/games/platformer/lvl.js
+++ public/games/platformer/lvl.js
@@ -188,10 +188,12 @@ lvl.prototype.exportPNG = function(){
 			console.log(this.decode[array[( i + 1) * ( j + 1 )]]);
 			drawing.onload = (function() {
 				var a = drawing;
-				var x = i;
-				var y = j;
+				var x = j;
+				var y = i;
 				return function(){
 					console.log(a);
+					console.log(x);
+					console.log(y);
   	 				context.drawImage(a, x * 25, y * 25, 25, 25);
 					count++;
 					console.log(count);
