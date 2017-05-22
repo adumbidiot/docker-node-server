7214c23ce7f388588ecda9e50f1c6376d088cd98
adumbidiot <nathaniel.daniel23@outlook.com>
2017-05-22 14:38:09 -0700

    Update popupManager.js

--- public/games/platformer/popupManager.js
+++ public/games/platformer/popupManager.js
@@ -1,11 +1,11 @@
 window.popupManager = function(){
-  this.base = document.createElement('div');
-  this.base.style.cssText = 'display:none;background-color: rgba(0, 0, 0, 0.5);position:fixed;top:0px;height:100%;width:100%;left:0px;z-index:999';
-  document.getElementsByTagName('body')[0].appendChild(this.base);
+	this.base = document.createElement('div');
+	this.base.style.cssText = 'display:none;background-color: rgba(0, 0, 0, 0.5);position:fixed;top:0px;height:100%;width:100%;left:0px;z-index:999';
+	document.getElementsByTagName('body')[0].appendChild(this.base);
   
-  this.contentHolder = document.createElement('div');
-  this.contentHolder.style.cssText = 'position:fixed;width:80%;height:80%;top:10%;left:10%;background-color:rgba(255, 255, 255, 1)';
-  this.base.appendChild(this.contentHolder);
+	this.contentHolder = document.createElement('div');
+	this.contentHolder.style.cssText = 'position:fixed;width:80%;height:80%;top:10%;left:10%;background-color:rgba(255, 255, 255, 1)';
+	this.base.appendChild(this.contentHolder);
 }
 
 //Opens popup, append specified content
