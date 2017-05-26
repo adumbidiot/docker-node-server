d7aae13a7de2ddd1c53068764d1beffea24c4fb3
adumbidiot <nathaniel.daniel23@outlook.com>
2017-05-26 11:22:34 -0700

    Create jsnes.js

---
+++ jsnes.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const handlebars = require('./handlebars')
+const app = express();
+
+handlebars.attach([app]);
+
+app.get('/', function(req, res){
+	res.render('jsnes');	
+});
+
+app.use(express.static('/public/games/jsnes'));
+
+module.exports = app;
