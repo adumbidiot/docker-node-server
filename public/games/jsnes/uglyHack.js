8b217ed23c5ea386dc2c269456fc03ff334dde1e
adumbidiot <nathaniel.daniel23@outlook.com>
2017-05-31 09:18:43 -0700

    Update uglyHack.js

--- public/games/jsnes/uglyHack.js
+++ public/games/jsnes/uglyHack.js
@@ -80,7 +80,7 @@ JSNES.Mappers[9].prototype.write = function(address, value) {
 };
 
 JSNES.Mappers[9].prototype.loadROM = function(rom) {
-    if (!this.rom.isValid()) {
+    if (!this.nes.rom.isValid()) {
         console.log("MMC2: Invalid ROM! Unable to load.");
         return;
     }
