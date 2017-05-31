2814a8caff1cc2dd890f42c6c86c3cb0d66a27b8
adumbidiot <nathaniel.daniel23@outlook.com>
2017-05-31 09:14:13 -0700

    Update uglyHack.js

--- public/games/jsnes/uglyHack.js
+++ public/games/jsnes/uglyHack.js
@@ -8,43 +8,98 @@ JSNES.Mappers[9].prototype = new JSNES.Mappers[0]();
 
 JSNES.Mappers[9].prototype.write = function(address, value) {
     console.log('Mapper_9: Writing');
-    this.loadRomBank(value, 0x8000);
-};
-
-/*
     if (address < 0x8000) {
         JSNES.Mappers[0].prototype.write.apply(this, arguments);
         return;
-    } else {
-        // Swap in the given PRG-ROM bank:
-        var prgbank1 = ((value & 0xF) * 2) % this.nes.rom.romCount;
-        var prgbank2 = ((value & 0xF) * 2 + 1) % this.nes.rom.romCount;
+    }else{
+        value &= 0xFF;
+        address &= 0xF000;
+        
+        switch (address >> 12) {
+            case 0xA: {
+                // Select 8k ROM bank at 0x8000
+                this.load8kRomBank(value, 0x8000);
+                return;
+                
+            }
+            case 0xB: {
 
-        this.loadRomBank(prgbank1, 0x8000);
-        this.loadRomBank(prgbank2, 0xC000);
+                // Select 4k VROM bank at 0x0000, $FD mode
+                latchLoVal1 = value;
+                if (latchLo == 0xFD) {
+                    this.loadVromBank(value, 0x0000);
+                }
+                
+                return;
+            }
+            case 0xC: {
 
+                // Select 4k VROM bank at 0x0000, $FE mode
+                latchLoVal2 = value;
+                if (latchLo == 0xFE) {
+                    this.loadVromBank(value, 0x0000);
+                }
+                return;
 
-        if (this.nes.rom.vromCount > 0) {
-            // Swap in the given VROM bank at 0x0000:
-            var bank = ((value >> 4) * 2) % (this.nes.rom.vromCount);
-            this.loadVromBank(bank, 0x0000);
-            this.loadVromBank(bank + 1, 0x1000);
-        }
+            }
+            case 0xD: {
+
+                // Select 4k VROM bank at 0x1000, $FD mode
+                latchHiVal1 = value;
+                if (latchHi == 0xFD) {
+                    this.loadVromBank(value, 0x1000);
+                }
+                return;
+            }
+            case 0xE: {
+                // Select 4k VROM bank at 0x1000, $FE mode
+                latchHiVal2 = value;
+                if (latchHi == 0xFE) {
+                    this.loadVromBank(value, 0x1000);
+                }
+                return;
+            }
+            case 0xF: {
+
+                // Select mirroring
+                
+                if ((value & 0x1) == 0) {
+                    // Vertical mirroring
+                    
+                    this.nes.getPpu().setMirroring(this.nes.rom.VERTICAL_MIRRORING);
+                } else {
+
+                    // Horizontal mirroring
+                    this.nes.getPpu().setMirroring(this.nes.rom.HORIZONTAL_MIRRORING);
+
+                }
+                return;
+            }
+                             }
     }
- */
+};
+
 JSNES.Mappers[9].prototype.loadROM = function(rom) {
-    if (!this.nes.rom.valid) {
-        alert("AOROM: Invalid ROM! Unable to load.");
+    if (!rom.isValid()) {
+        console.log("MMC2: Invalid ROM! Unable to load.");
         return;
     }
-
+    
+    // Get number of 8K banks:
+    var num_8k_banks = rom.getRomBankCount() * 2;
     // Load PRG-ROM:
-    this.loadPRGROM();
+    this.load8kRomBank(0, 0x8000);
+    this.load8kRomBank(num_8k_banks - 3, 0xA000);
+    this.load8kRomBank(num_8k_banks - 2, 0xC000);
+    this.load8kRomBank(num_8k_banks - 1, 0xE000);
 
     // Load CHR-ROM:
-    this.loadCHRROM();
+    loadCHRROM();
+
+    // Load Battery RAM (if present):
+    loadBatteryRam();
 
     // Do Reset-Interrupt:
-    this.nes.cpu.requestIrq(this.nes.cpu.IRQ_RESET);
+    this.nes.getCpu().requestIrq(CPU.IRQ_RESET);
 };
 
